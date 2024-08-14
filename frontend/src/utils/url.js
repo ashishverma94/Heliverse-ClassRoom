14a461ebcdd123a390e7a4473b83360ca155c333
ashishverma94 <hariashish1@gmail.com>
2024-08-14 19:10:02 +0530

    deployed backend on render and changed DOMAIN_URL

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,4 +1,4 @@
-// const DOMAIN_URL = 'https://cuvette-notes-backend-1.onrender.com' ;
-const DOMAIN_URL = "http://localhost:3000"
+const DOMAIN_URL = 'https://heliverse-classroom-1.onrender.com/' ;
+// const DOMAIN_URL = "http://localhost:3000"
 
 export default DOMAIN_URL ;
